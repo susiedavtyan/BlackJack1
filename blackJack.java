0b6219881f9a8fabf0a11a219ea9563b6103c97b
susiedavtyan <susie_davtyan@yahoo.com>
2015-02-18 11:22:14 -0800

    Create blackJack.java

---
+++ blackJack.java
@@ -0,0 +1,44 @@
+public class blackJack extends Hand {
+ 
+     public int getValue() {
+ 
+
+         int val;      // The value computed for the hand.
+         boolean ace;  // This will be set to true if the
+                       //   hand contains an ace.
+         int cards;    // Number of cards in the hand.
+
+         val = 0;
+         ace = false;
+         cards = getCardCount();
+
+         for ( int i = 0;  i < cards;  i++ ) {
+             Card card;    
+             int cardVal;  
+             card = getCard(i);
+             cardVal = card.getValue();  // The normal value, 1 to 13.
+             if (cardVal > 10) {
+                 cardVal = 10;   // For a Jack, Queen, or King.
+             }
+             if (cardVal == 1) {
+                 ace = true;     // There is at least one ace.
+             }
+             val = val + cardVal;
+          }
+
+             // Now, val is the value of the hand, counting any ace as 1.
+             // If there is an ace, and if changing its value from 1 to 
+             // 11 would leave the score less than or equal to 21,
+             // then do so by adding the extra 10 points to val. 
+
+          if ( ace == true  &&  val + 10 <= 21 )
+              val = val + 10;
+
+          return val;
+
+     }
+
+	          
+	}  
+ 
+
